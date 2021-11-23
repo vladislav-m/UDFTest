2d840a34a94ebf5ef420eee72961f9443267b1cd
Vladislav Myakotin <vladislav.myakotin@gmail.com>
2021-11-23 17:21:32 +0300

    Migrate sign in to TCA

--- SignInUI/SignInView.swift
+++ SignInUI/SignInView.swift
@@ -1,35 +1,46 @@
 import SwiftUI
 import Combine
+import ComposableArchitecture
 
 public struct SignInView: View {
-    @ObservedObject
-    private var viewModel: SignInViewModel
+    private var store: Store<SignInState, SignInAction>
 
-    public init(viewModel: SignInViewModel) {
-        self.viewModel = viewModel
+    public init(
+        store: Store<SignInState, SignInAction>
+    ) {
+        self.store = store
     }
 
     public var body: some View {
-        Form {
-            TextField("Username", text: $viewModel.state.username)
-            TextField("Password", text: $viewModel.state.password)
+        WithViewStore(store) { viewStore in
+            Form {
+                TextField("Username", text: viewStore.binding(
+                    get: \.username,
+                    send: SignInAction.setUsername
+                ))
+                TextField("Password", text: viewStore.binding(
+                    get: \.password,
+                    send: SignInAction.setPassword
+                ))
 
-            Button {
-                viewModel.send(.signIn)
-            } label : {
-                Text("Sign in")
-                if viewModel.state.isSigningIn {
-                    ProgressView()
+                Button {
+                    viewStore.send(.signIn)
+                } label : {
+                    Text("Sign in")
+                    if viewStore.isSigningIn {
+                        ProgressView()
+                    }
                 }
             }
         }
     }
 }
 
-public struct SignInState {
+public struct SignInState: Equatable {
     public var username: String = ""
     public var password: String = ""
     public var isSigningIn: Bool = false
+    public var errorMessage: String?
 
     public init(
         username: String = "",
@@ -42,42 +53,72 @@ public struct SignInState {
     }
 }
 
-public enum SignInAction {
+public enum SignInAction: Equatable {
+    case setUsername(String)
+    case setPassword(String)
+
     case signIn
-    case signedIn
-    case error(AuthService.Error)
+    case signedIn(Result<Bool, AuthService.Error>)
 }
 
-public final class SignInViewModel: ObservableObject {
-    @Published var state: SignInState
+public struct SignInEnvironment {
+    private var signIn: (_ credentials: UserCredentials) -> AnyPublisher<Void, AuthService.Error>
 
-    private var cancellables: Set<AnyCancellable> = []
-
-    private let authService: AuthService
-
-    public init(state: SignInState = .init(), authService: AuthService = .shared) {
-        self.state = state
-        self.authService = authService
+    public init(
+        signIn: @escaping (_ credentials: UserCredentials) -> AnyPublisher<Void, AuthService.Error>
+    ) {
+        self.signIn = signIn
     }
 
-    func send(_ action: SignInAction) {
+    public func signIn(credentials: UserCredentials) -> Effect<SignInAction, Never> {
+        return self.signIn(credentials)
+            .map { true }
+            .catchToEffect(SignInAction.signedIn)
+    }
+}
+
+extension SignInEnvironment {
+    static var live: Self {
+        SignInEnvironment(
+            signIn: AuthService.shared.signIn
+        )
+    }
+
+    static var succeeding: Self {
+        SignInEnvironment(
+            signIn: { _ in CurrentValueSubject(()).eraseToAnyPublisher() }
+        )
+    }
+}
+
+extension Reducer where State == SignInState, Action == SignInAction, Environment == SignInEnvironment {
+    static let signIn = Reducer { state, action, env in
         switch action {
+        case let .setUsername(username):
+            state.username = username
+            return .none
+        case let .setPassword(password):
+            state.password = password
+            return .none
+
         case .signIn:
             state.isSigningIn = true
+            state.errorMessage = nil
 
-            authService.signIn(credentials: .init(
+            let credentials = UserCredentials(
                 username: state.username,
                 password: state.password
-            )).sink { completion in
-                if case let .failure(error) = completion {
-                    self.send(.error(error))
-                }
-            } receiveValue: {
-                self.send(.signedIn)
-            }.store(in: &cancellables)
+            )
 
-        case .signedIn:
+            return env.signIn(credentials: credentials)
+
+        case .signedIn(.success):
             state.isSigningIn = false
+            return .none
+        case let .signedIn(.failure(error)):
+            state.isSigningIn = false
+            state.errorMessage = "Попробуйте снова"
+            return .none
         }
     }
 }
@@ -90,7 +131,7 @@ public struct UserCredentials {
 public final class AuthService {
     public static let shared: AuthService = .init()
 
-    public enum Error: Swift.Error {
+    public enum Error: Swift.Error, Equatable {
 
     }
 
@@ -109,7 +150,11 @@ public final class AuthService {
 #if DEBUG
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView(viewModel: .init())
+        SignInView(store: Store(
+            initialState: .init(),
+            reducer: .signIn,
+            environment: .succeeding
+        ))
     }
 }
 #endif
